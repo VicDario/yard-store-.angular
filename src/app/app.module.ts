93f560e0d0bf62a94884d61fa5609f5e5cff4e15
VicDario <26585356+VicDario@users.noreply.github.com>
2023-08-27 21:15:35 -0400

    feat: quicklink preload strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { QuicklinkModule } from 'ngx-quicklink';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    QuicklinkModule,
   ],
   providers: [
     Title,
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WebsiteRoutingModule } from './website-routing.module';
+import { QuicklinkModule } from 'ngx-quicklink';
 
 import { NavComponent } from './components/nav/nav.component';
 
@@ -9,6 +10,6 @@ import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [NavComponent, LayoutComponent],
-  imports: [CommonModule, WebsiteRoutingModule],
+  imports: [CommonModule, WebsiteRoutingModule, QuicklinkModule],
 })
 export class WebsiteModule {}
