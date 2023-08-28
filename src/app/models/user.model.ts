03f0f6792e7dabb48bc9bd263ee1bd914abaaf2f
VicDario <26585356+VicDario@users.noreply.github.com>
2023-08-27 21:20:12 -0400

    style: format

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,15 +1,15 @@
 export interface Credentials {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 export interface Login {
-    access_token: string;
+  access_token: string;
 }
 export interface User {
-    id: string;
-    email: string;
-    password: string;
-    name: string;
+  id: string;
+  email: string;
+  password: string;
+  name: string;
 }
 
 export interface CreateUserDTO extends Omit<User, 'id'> {}
